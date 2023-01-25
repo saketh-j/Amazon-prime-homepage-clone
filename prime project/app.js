438440eb8df1c8866935de4d164a7aef4d265d75
saketh <109161554+saketh-j@users.noreply.github.com>
2023-01-25 12:08:53 +0530

    Add files via upload

---
+++ prime project/app.js
@@ -0,0 +1,20 @@
+
+function show_list() {
+    var courses = document.getElementById("courses_id");
+
+    if (courses.style.display == "block") {
+        courses.style.display = "none";
+    } else {
+        courses.style.display = "block";
+    }
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown_button')) {
+        document.getElementById('courses_id')
+            .style.display = "none";
+    }
+    else {
+        courses.style.display = "block";
+    }
+}   
+
